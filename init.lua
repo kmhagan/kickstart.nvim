b1b0bdca9991c1eee301744c155028b4debe0534
kh448p <kh448p@exo.att.com>
2024-03-21 12:58:06 -0700

    Initial customizations

--- init.lua
+++ init.lua
@@ -539,16 +539,16 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        gopls = {},
+        pyright = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        tsserver = {},
         --
 
         lua_ls = {
@@ -616,6 +616,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
+        go = { 'goimports', 'gofmt' }
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
@@ -836,7 +837,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,15 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+vim.keymap.set('n', '<leader>f', ':lua vim.lsp.buf.format()<CR>', { desc = 'To [f]ormat file' })
+
+local function format_buffer()
+  local opts = {
+    timeout_ms = 1000,
+  }
+  vim.lsp.buf.format(opts)
+end
+
+vim.api.nvim_create_user_command('Fmt', format_buffer, {})
+
 return {}
